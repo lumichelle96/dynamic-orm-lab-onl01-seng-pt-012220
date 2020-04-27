0393d47a32a01a87746a3c78c85ee65c371c528c
Michelle Lu <lu.michelle96@icloud.com>
2020-04-27 23:30:29 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,6 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  
   def self.table_name
     self.to_s.downcase.pluralize
   end
@@ -24,5 +25,21 @@ class InteractiveRecord
     end
   end
   
-  def table_info.each do |column|
+  def table_name_for_insert
+    self.class.table_name
+  end
+  
+  def col_name_for_insert
+    self.class.column_names.delete_if{|col| col == "id"}.join(", ")
+  end
+  
+  def values_for_insert
+    values  = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+  
+  
     
